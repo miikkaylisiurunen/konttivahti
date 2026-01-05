0e7218ebca81167cda30520ad1af1a9da4a4dc2f
Miikka Ylisiurunen <miikka.ylisiurunen@gmail.com>
2026-01-05 23:40:13 +0200

    Add tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
-import styles from './App.module.css';
-
 function App() {
   return (
-    <div className={styles.app}>
-      <h1 className={styles.title}>Hello</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen text-foreground">
+      <h1 className="text-4xl font-bold mb-4">Hello</h1>
     </div>
   );
 }
